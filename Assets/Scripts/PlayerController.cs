a5461b82c88f83ee3530585a26fc26c46015e235
Douglas Urner <dlu@canishe.com>
2020-02-23 14:33:18 -0800

    Player controls

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+  [SerializeField] string moveAxis = "Vertical";
+  [SerializeField] float speed = 10;
+
+  Rigidbody playerRb;
+  GameObject focalPoint;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    playerRb = GetComponent<Rigidbody>();
+    focalPoint = GameObject.Find("Focal Point");
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    var playerMovement = Input.GetAxis(moveAxis);
+    playerRb.AddForce(
+      focalPoint.transform.forward * playerMovement * speed * Time.deltaTime);
+  }
+}
---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+  [SerializeField] string rotAxis = "Horizontal";
+  [SerializeField] float rotSpeed = 30;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    var rotInput = Input.GetAxis(rotAxis);
+    transform.Rotate(Vector3.up, rotInput * rotSpeed * Time.deltaTime);
+  }
+}

3d2112e12443aeab203904903dd480ad49c0e739
Douglas Urner <dlu@canishe.com>
2020-02-23 20:38:30 -0800

    Set up opponent and spawn manager

---
+++ Assets/Scripts/OpponentController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpponentController : MonoBehaviour
+{
+  [SerializeField] float speed = 5.0f;
+
+  Rigidbody opponentRb;
+  GameObject player;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    opponentRb = GetComponent<Rigidbody>();
+    player = GameObject.Find("Player");
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    var lookDirection =
+      (player.transform.position - transform.position).normalized;
+
+    // Lesson does not use Time.deltaTime - why?
+    opponentRb.AddForce(lookDirection * speed * Time.deltaTime);
+  }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+  [SerializeField] GameObject[] opponentPrefabs;
+  [SerializeField] float spawnRange = 9.0f;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    var index = Random.Range(0, opponentPrefabs.Length);
+    var prefab = opponentPrefabs[index];
+
+    Instantiate(prefab, SpawnPosition(), prefab.transform.rotation);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  Vector3 SpawnPosition()
+  {
+    var spawnPosX = Random.Range(-spawnRange, spawnRange);
+    var spawnPosZ = Random.Range(-spawnRange, spawnRange);
+
+    return new Vector3(spawnPosX, 0, spawnPosZ);
+  }
+}
